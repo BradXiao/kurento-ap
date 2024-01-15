5ff69b90cb993741fc6a57508478eacb37cf51ac
brad <brad.821228@gmail.com>
2024-01-15 11:00:55 +0800

    feat: detect OS

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,6 +5,17 @@ let controller = null;
 window.onload = function () {
     ui.init();
     controller = new wsController.Controller();
+    var os = utils.getPlatform();
+    if (os === "MacOS" || os === "iOS") {
+        ui.showMessage(
+            "The demo is not supported on iOS yet.",
+            () => {
+                $("body").html("");
+            },
+            "error",
+            "Exit"
+        );
+    }
 };
 
 window.onbeforeunload = function () {
--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,1 +1,28 @@
 export const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+/**
+ * @returns {String}
+ * https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
+ */
+export function getPlatform() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ["macOS", "Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+        iosPlatforms = ["iPhone", "iPad", "iPod"];
+    let os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = "MacOS";
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = "iOS";
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = "Windows";
+    } else if (/Android/.test(userAgent)) {
+        os = "Android";
+    } else if (/Linux/.test(platform)) {
+        os = "Linux";
+    }
+
+    return os;
+}
